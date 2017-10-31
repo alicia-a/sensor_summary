a6e0b14ced28a33796cd6dacc7f589c22267da2d
alicia-a <msleesh@gmail.com>
2017-11-01 09:58:18 +1000

    Added summarise by-minute and by-hour functions

--- Functions.R
+++ Functions.R
@@ -9,7 +9,9 @@ library(tidyverse)
 library(lubridate)
 
 ####################################
-# Function_1 - trim and add needed time variables
+# Trim n Time function
+
+# Argument "File loc" = location of the file you're reading in, relative to the working directory.
 
 trim_n_time <- function(file_loc){
   
@@ -43,3 +45,52 @@ trim_n_time <- function(file_loc){
   return(sens_MtoM)
 }
 
+
+
+####################################
+# Average reading per minute
+
+# Argument "dat" = data you want to evaluate
+# Argument "streams" = columns you want included in the summary
+
+avg_24h_minute <- function(dat, streams){
+  
+avg_by_min <- dat %>% 
+  group_by(min_of_day) %>%
+  summarise_at(streams, mean, na.rm = T) %>%
+  mutate_if(is.numeric, funs(round(.,2)))
+
+avg_by_min$hr_of_day <- as.factor(rep(1:24, each = 60))
+
+return(avg_by_min)
+}
+
+
+
+####################################
+# Average reading per hour
+
+
+# Argument "dat" = data you want to evaluate
+# Argument "streams" = columns you want included in the summary
+
+avg_24h_hour <- function(streams){
+
+avg_lux_by_hr <- avg_24h_minute(streams) %>%
+  group_by(hr_of_day) %>%
+  summarise_at(streams, mean, na.rm = T) %>%
+  mutate_if(is.numeric, funs(round(.,2)))
+
+}
+
+
+
+
+####################################
+# Average time over threshold - NOT WORKING YET (move across from original script)
+
+# Argument dat = data to evaluate
+# Argument var_eval = variable to evaluate
+# Argument thresholds = vector of thresholds to evaluate (add later)
+
+# Use either mutate_at or summarise_at
